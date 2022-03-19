7461867c5ad4e1f12a2fdab2d62097a3071d5721
Rutvik <rutvikpatel1092@gmail.com>
2022-03-19 15:34:19 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const weatherData = require('../utils/weatherData');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 const publicStaticDirPath = path.join(__dirname, '../public')
 
